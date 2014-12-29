d9d4212d37674be71dc1343a6fc459addf6432f2
MadkaT182 <madcat182@gmail.com>
2014-12-29 11:29:26 -0500

    Se desactiva el plugin autosave del ckeditor

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -30,6 +30,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscrip, Source, Anchor, UIColor';
 
+	config.removePlugins = 'autosave';
+	
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
